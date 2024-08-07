f04a41c87ebaac103c780557bbca0b44c56efc5d
VeeraVasanth11 <158580254+VeeraVasanth11@users.noreply.github.com>
2024-08-07 17:07:03 +0530

    Add files via upload

---
+++ RandomPasswordGen/script.js
@@ -0,0 +1,23 @@
+const pass=document.getElementById("pass");
+const btn=document.querySelector("button");
+const length=12;
+const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase="abcdefghijklmnopqrstuvwxyz";
+const numbers="0123456789";
+const symbol="!@#$%^&*()_+{}[]";
+const allChars=upperCase+lowerCase+numbers+symbol;
+function generatePass(){
+    let password="";
+    password+=upperCase[Math.floor(Math.random()*upperCase.length)];
+    password+=lowerCase[Math.floor(Math.random()*lowerCase.length)];
+    password+=numbers[Math.floor(Math.random()*numbers.length)];
+    password+=symbol[Math.floor(Math.random()*symbol.length)];
+    while(length>password.length){
+          password+=allChars[Math.floor(Math.random()*allChars.length)];
+    }
+    pass.value=password;
+}
+btn.addEventListener('click',()=>{
+ generatePass();
+    
+})
